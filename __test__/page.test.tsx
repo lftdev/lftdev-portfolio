31969ccd3a417cf56d90440c7efbec66a1b39056
lftdev <torres.lf21@gmail.com>
2023-07-24 14:03:34 -0300

    Create and test HomePage

--- __test__/page.test.tsx
+++ __test__/page.test.tsx
@@ -1,5 +1,5 @@
-import {cleanup, render, screen} from '@testing-library/react'
-import {afterEach, describe, it} from 'vitest'
+import {cleanup, fireEvent, render, screen} from '@testing-library/react'
+import {afterEach, describe, expect, it} from 'vitest'
 import React from 'react'
 import HomePage from '../app/page'
 
@@ -8,8 +8,28 @@ describe('Home page', () => {
   it('should render correctly;', () => {
     render(<HomePage/>)
   })
-  it('should render main title;', () => {
+  it('should display a greet;', () => {
     render(<HomePage/>)
-    screen.getByText('Nice to meet you! I\'m Lucas Torres, Frontend Developer.')
+    screen.getByText('Nice to meet you!')
+  })
+  it('should display a presentation as heading of level 1;', () => {
+    render(<HomePage/>)
+    const h1 = screen.getByRole('heading', { level: 1 })
+    expect(h1.innerText).toBe('My name is Lucas and I\'m Frontend Developer.')
+  })
+  it('should display a paragraph;', () => {
+    render(<HomePage/>)
+    const p = screen.getByRole('paragraph')
+    console.log(p.innerText)
+    expect(p.innerText).toBe('But, I know, you are here to see the proof of it so...')
+  })
+  it('should display a call to action link;', () => {
+    render(<HomePage/>)
+    const link = screen.getByRole('link')
+    expect(link.innerText).toBe('Let\'s see my works!')
+  })
+  it('should lead to repos page when click on call to action link;', () => {
+    render(<HomePage/>)
+    expect(screen.getByText('Let\'s see my works!').getAttribute('href')).toBe('/repos')
   })
 })
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function HomePage() {
-  return <h1>{'Nice to meet you! I\'m Lucas Torres, Frontend Developer.'}</h1>
+import Link from 'next/link'
+
+export default function HomePage (): JSX.Element {
+  // Redundant role used for paragraph element was included as it was not being detected by tests.
+  return (
+    <main className='p-5 h-full'>
+      <div className='grid gap-2'>
+        <h2 className='text-3xl text-pink-500'>Nice to meet you!</h2>
+        <h1 className='text-4xl'>
+          My name is<span className='font-bold text-purple-600'> Lucas </span>and I'm Frontend Developer.
+        </h1>
+      </div>
+      <div className='grid place-items-center h-56'>
+        <p className='text-lg' role='paragraph'>But, I know, you are here to see the proof of it so...</p>
+        <Link className='p-2 rounded-md bg-purple-600 font-bold' href='/repos'>Let's see my works!</Link>
+      </div>
+    </main>
+  )
 }
