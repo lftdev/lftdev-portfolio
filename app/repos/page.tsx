73331bea8902f9579ced05a166e7449fee27d1d0
lftdev <torres.lf21@gmail.com>
2023-07-24 16:18:11 -0300

    Create and test general structure for ReposPage

---
+++ app/repos/page.tsx
@@ -0,0 +1,15 @@
+export default function ReposPage (): JSX.Element {
+  return (
+    <>
+      <h1>Take a look at my repositories</h1>
+      <p role='paragraph'>Here you'll find my training as web developer and other projects.</p>
+      <main>
+        <ul>
+          <li>
+            <article></article>
+          </li>
+        </ul>
+      </main>
+    </>
+  )
+}
