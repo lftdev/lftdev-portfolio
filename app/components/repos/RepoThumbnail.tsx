72411f1f64828ad82a779e10be3a55fbc848ca4f
lftdev <torres.lf21@gmail.com>
2023-07-27 17:09:22 -0300

    Remove useEffect from RepoThumbnail

--- app/components/repos/RepoThumbnail.tsx
+++ app/components/repos/RepoThumbnail.tsx
@@ -1,11 +1,9 @@
 'use client'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 export default function RepoThumb (props: { thumbnail: string }): JSX.Element {
   const { thumbnail } = props
   const [noImgError, setNoImgError] = useState(false)
-  useEffect(() => {
-    setNoImgError(false)
-  }, [thumbnail])
+
   return <Image onError={() => setNoImgError(true)} src={noImgError ? '/images/repo-default-view.svg' : thumbnail} width={150} height={150} alt='Project live site thumbnail' />
 }
