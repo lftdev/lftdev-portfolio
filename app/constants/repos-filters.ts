fdc964b93dcdcb5721efdebbd6c8637ce5448e7d
lftdev <torres.lf21@gmail.com>
2023-08-08 18:55:18 -0300

    feat: :sparkles: Separate Hobby Projects in Repos Page

--- app/constants/repos-filters.ts
+++ app/constants/repos-filters.ts
@@ -1,1 +1,2 @@
 export const PROFESSIONAL_PROJECTS = ['lftdev-portfolio']
+export const PYTHON_PROJECTS = ['imgcom', 'pythaur']
--- app/repos/page.tsx
+++ app/repos/page.tsx
@@ -1,7 +1,7 @@
 import Heading from '@/components/html-customs/Heading'
 import ReposList from '@/components/repos/ReposList'
 import { getRepositories } from '../services/repo-fetching'
-import { PROFESSIONAL_PROJECTS } from '../constants/repos-filters'
+import { PROFESSIONAL_PROJECTS, PYTHON_PROJECTS } from '../constants/repos-filters'
 
 export default function ReposPage (): JSX.Element {
   const repositories = getRepositories()
@@ -14,13 +14,27 @@ export default function ReposPage (): JSX.Element {
         <p role='paragraph'>Here, you'll find my training as <span className='font-bold'>web developer</span> and other works I consider my <span className='font-bold'>hobby projects</span>.</p>
       </div>
       <Heading level={2} className='text-purple-600'>
-        Web Projects
+        Web Development Projects
       </Heading>
       <main>
         <ReposList title='Professional works' list={repositories} filter={list => list.filter(repo => PROFESSIONAL_PROJECTS.includes(repo.name))} />
       </main>
       <section>
-        <ReposList title='Training projects' list={repositories} filter={list => list.filter(repo => !PROFESSIONAL_PROJECTS.includes(repo.name))} />
+        <ReposList
+          title='Training projects' list={repositories}
+          filter={list => list.filter(repo => !PROFESSIONAL_PROJECTS.includes(repo.name) &&
+          !PYTHON_PROJECTS.includes(repo.name))}
+        />
+      </section>
+      <Heading level={2} className='text-purple-600'>
+        Hobby projects
+      </Heading>
+      <section>
+        <ReposList
+          title='Python projects' list={repositories}
+          filter={list => list.filter(repo =>
+            PYTHON_PROJECTS.includes(repo.name))}
+        />
       </section>
     </div>
   )
