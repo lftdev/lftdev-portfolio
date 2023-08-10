5e19e8cb07bedd443a1b52bbd509aa76192b8888
lftdev <torres.lf21@gmail.com>
2023-08-10 17:57:06 -0300

    Change ReposPreview buttons style

--- app/components/repos/RepoPreview.tsx
+++ app/components/repos/RepoPreview.tsx
@@ -7,12 +7,12 @@ export default function RepoPreview (props: { repository: Repository }): JSX.Ele
   return (
     <article className='group w-full h-44 rounded-lg border border-gray-700 overflow-hidden hover:scale-105 transition-all duration-150'>
       <RepoThumbnail alt={repository.name + ' thumbnail'} thumbnail={repository.thumbnail} />
-      <footer className='relative bottom-1/2 lg:bottom-72 lg:group-hover:bottom-1/2 transition-all duration-150'>
+      <footer className='relative bottom-1/2'>
         <div className='flex justify-between px-2'>
-          <Link title='Deploy' href={repository.deploy} target='_blank' className='p-5 rounded-full bg-purple-500'>
+          <Link title='Deploy' href={repository.deploy} target='_blank' className='flex justify-center items-center gap-2 p-5 rounded-full font-bold bg-purple-500 group-hover:after:content-["Deploy"] transition-all duration-150'>
             {WINDOW_ICON}
           </Link>
-          <Link title='View code' href={repository.htmlUrl} target='_blank' className='p-5 rounded-full bg-gray-800'>
+          <Link title='View code' href={repository.htmlUrl} target='_blank' className='flex justify-center items-center gap-2 p-5 rounded-full font-bold bg-gray-800 group-hover:before:content-["Source"] transition-all duration-150'>
             {CODE_ICON}
           </Link>
         </div>
